8da1e61e830ea473ba1dd2e6393987f11675a9ef
SergioDanielGalvan <55294440+SergioDanielGalvan@users.noreply.github.com>
2023-06-14 14:28:20 -0300

    Add files via upload
    Correcciones de checks de coordenadas, constructor y metodo getCasillero

--- Tablero3D.cpp
+++ Tablero3D.cpp
@@ -19,11 +19,11 @@ Tablero3D::Tablero3D() {
 				Casillero * casillero;
 				casillero = new Casillero(x, y, z);
 				// casillero = new Casillero();
-				subelemento->agregar( casillero);
+				subelemento->agregarFinal( casillero);
 			}
-			elemento->agregar(subelemento);
+			elemento->agregarFinal(subelemento);
 		}
-		this->tablero->agregar(elemento);
+		this->tablero->agregarFinal(elemento);
 	}
 }
 
@@ -91,11 +91,9 @@ Casillero * Tablero3D::getCasillero( int x, int y, int z ) {
 	}
 	Lista < Lista < Casillero * > * > * elemento;
 	Lista < Casillero * > * subelemento;
+
 	// Traslación de coordenadas
 	x--; y--; z--;
-	if ( z == 18 ) {
-		z = z;
-	}
 	elemento = this->tablero->obtenerDato( x );
 	subelemento = elemento->obtenerDato( y );
 	Casillero * Casillero;
@@ -116,13 +114,13 @@ int Tablero3D::getSize_z() {
 }
 
 bool Tablero3D::checkCoordenadas( int x, int y, int z ) {
-	if ( x < 0 || x > this->size_x - 1 ) {
+	if ( x < 1 || x > this->size_x ) {
 		return false;
 	}
-	if ( y < 0 || y > this->size_y - 1) {
+	if ( y < 1 || y > this->size_y ) {
 		return false;
 	}
-	if ( z < 0 || z > this->size_z - 1) {
+	if ( z < 1 || z > this->size_z ) {
 		return false;
 	}
 	return true;
