78cffcb428c5628af39dc9fc85025cb82b5ff75d
SergioDanielGalvan <55294440+SergioDanielGalvan@users.noreply.github.com>
2023-06-14 19:18:23 -0300

    Add files via upload

--- MostrarTablero.h
+++ MostrarTablero.h
@@ -1,10 +1,18 @@
+#include "Tablero3D.h"
 #include "MostrarCasillero.h"
 
 class MostrarTablero
 {
 public:
-	MostrarTablero();
+	MostrarTablero( Tablero3D * );
 	~MostrarTablero();
+	void imprimir( string );
+	void setNombreArchivo( string );
+	void setJugador( Jugador * );
+	void setJugador( int );
 private:
+	Tablero3D * tablero;
+	string nombreArchivo;
+	Jugador jugador;
 };
 
